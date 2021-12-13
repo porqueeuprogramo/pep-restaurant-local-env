7cd7dac9cc164c3db4e7efd6ff29d72deee108b4
porqueeuprogramo <porqueeuprogramo@gmail.com>
2021-12-13 23:21:16 +0000

    postgres keycloak and sonarqube

--- postgres-db/create_db.sh
+++ postgres-db/create_db.sh
@@ -2,18 +2,18 @@
 set -e
 
 POSTGRES="psql --username ${POSTGRES_USER}"
-echo "Creating user ${PEP_DB_USERNAME} with password"
 
+echo "Creating user ${PEP_DB_USERNAME} with password"
 $POSTGRES <<EOSQL
 CREATE USER ${PEP_DB_USERNAME} ENCRYPTED PASSWORD '${PEP_DB_PASSWORD}';
 EOSQL
-echo "Creating database: ${PEP_KEYCLOAK_DB}"
 
+echo "Creating database: ${PEP_KEYCLOAK_DB}"
 $POSTGRES <<EOSQL
 CREATE DATABASE ${PEP_KEYCLOAK_DB} OWNER ${PEP_DB_USERNAME};
 EOSQL
-echo "Creating database: ${PEP_SONAR_DB}"
 
+echo "Creating database: ${PEP_SONAR_DB}"
 $POSTGRES <<EOSQL
 CREATE DATABASE ${PEP_SONAR_DB} OWNER ${PEP_DB_USERNAME};
 EOSQL
